054d7f0abe94285c06a9ad76d76de4ab00dfb6e1
Sergey Sharaev <sega-sharaev@yandex.ru>
2025-02-02 15:06:06 +0300

    m30_hw1: Тест доступности GET роутов.

--- test/test_recipes.py
+++ test/test_recipes.py
@@ -1,2 +1,17 @@
-def test_route_status() -> None:
-    assert True
+import pytest
+from fastapi.testclient import TestClient
+
+from src.main import app
+
+
+client: TestClient = TestClient(app)
+
+
+@pytest.mark.parametrize('route', [
+    '/recipes',
+    '/recipes/1'
+])
+def test_route_status(route: str) -> None:
+    response = client.get(route)
+
+    assert response.status_code == 200
