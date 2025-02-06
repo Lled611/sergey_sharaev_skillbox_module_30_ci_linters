cb3ddf0e4b9638f248f4f098eaba60b4dca44822
Sergey Sharaev <sega-sharaev@yandex.ru>
2025-02-06 13:31:42 +0300

    m30_hw1: test_recipes.py прошел проверку линтерами black, isort, flake8.

--- test/test_recipes.py
+++ test/test_recipes.py
@@ -4,27 +4,20 @@ from httpx import AsyncClient
 
 @pytest.mark.asyncio
 async def test_create_recipe(client: AsyncClient) -> None:
-    response = await client.post('/recipes', json={
-        "name": "bread",
-        "description": "bread",
-        "cooking_time": 1,
-        "ingredients": [
-            {
-                "product": {
-                "name": "bread"
-            },
-                "count": 1
-            }
-        ]
-    })
+    response = await client.post(
+        "/recipes",
+        json={
+            "name": "bread",
+            "description": "bread",
+            "cooking_time": 1,
+            "ingredients": [{"product": {"name": "bread"}, "count": 1}],
+        },
+    )
 
     assert response.status_code == 204
 
 
-@pytest.mark.parametrize('route', [
-    '/recipes',
-    '/recipes/1'
-])
+@pytest.mark.parametrize("route", ["/recipes", "/recipes/1"])
 @pytest.mark.asyncio
 async def test_route_status(client: AsyncClient, route: str) -> None:
     response = await client.get(route)
